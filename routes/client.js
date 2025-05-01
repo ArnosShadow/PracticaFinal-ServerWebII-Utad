4dd9e801d46e18171f551df46ed9573ccf4ca9b4
Arnos <fj.jaraba.estevez@gmail.com>
2025-05-01 19:50:30 +0200

    Se ha incluido el getArchivedClients al router

--- routes/client.js
+++ routes/client.js
@@ -5,12 +5,13 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const handleValidator = require("../utils/handleValidator");
 const { clientValidator } = require("../validator/clientValidator");
 
-const {createClient,getClients,getClientById,updateClient,deleteClient,restoreClient} = require("../controllers/client");
+const {createClient,getClients,getClientById,updateClient,deleteClient,restoreClient, getArchivedClients} = require("../controllers/client");
 
 
 
 router.post("/",authMiddleware, clientValidator, handleValidator, createClient);
 router.get("/",authMiddleware, getClients);
+router.get("/archivados", getArchivedClients);
 router.get("/:id",authMiddleware, getClientById);
 router.put("/:id",authMiddleware, clientValidator, handleValidator, updateClient);
 router.delete("/:id",authMiddleware, deleteClient);
